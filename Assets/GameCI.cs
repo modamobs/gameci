b2574cfb438930dc922337250282c038c2ddc84e
modamobs <88258985+modamobs@users.noreply.github.com>
2022-03-12 21:11:20 +0900

    build test4

---
+++ Assets/GameCI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameCI : MonoBehaviour
+{
+
+    public Text test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        test.text = "123123123";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
